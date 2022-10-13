07fd05599226c7b41349b45b6b38930a4eff7c16
Mauro Canto <maucansa@gmail.com>
2022-10-13 18:50:57 +0200

    Addition of elapsed time and graphical view

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from PIL import Image
 import os
 import shutil
+import matplotlib.pyplot as plt
+import time
+
+starting_time = time.time()
+n_files = 0
 
 for file in os.listdir("./originals"):
     name, extension = file.split(".")
@@ -8,6 +13,7 @@ for file in os.listdir("./originals"):
     if extension in ['jpg', 'jpeg', 'png']:
         picture = Image.open("./originals/" + file)
         picture.save("./compressions/compressed_" + file, optimize = True, quality = 75)
+        n_files += 1
 
     else:
         shutil.copy("./originals/" + file, "./unable_to_compress/" + file)
@@ -29,6 +35,20 @@ before_compression = size_originals / (1024 * 1024)
 after_compression = (size_compressions + size_unable) / (1024 * 1024)
 reduction = (after_compression - before_compression) / before_compression * 100
 
+print('\n')
+print('{} files compressed in {:.2f} seconds'.format(n_files, time.time() - starting_time))
+print('\n')
 print('Size before compression: {:.2f} MB'.format(before_compression))
 print('Size after compression:  {:.2f} MB'.format(after_compression))
 print('Compression percentage:  {:.2f} %'.format(reduction))
+
+x = ['Before Compression', 'After Compression']
+y = [before_compression, after_compression]
+
+plt.bar(x, y)
+plt.title('Sizes before and after compression')
+
+for i, value in enumerate(y):
+    plt.text(i, value/2, str(round(value, 2)) + ' MB', fontweight = 'bold')
+
+plt.show()
